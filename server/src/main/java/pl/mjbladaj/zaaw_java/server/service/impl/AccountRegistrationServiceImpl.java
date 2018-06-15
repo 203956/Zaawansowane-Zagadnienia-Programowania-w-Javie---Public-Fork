c196fa7568bc709746846363c9ea26b4b2079280
Marcin Jaworski <m.a.jaworski96@gmail.com>
2018-06-15 15:51:40 +0200

    AccountRegistrationServiceImpl tests

--- server/src/main/java/pl/mjbladaj/zaaw_java/server/service/impl/AccountRegistrationServiceImpl.java
+++ server/src/main/java/pl/mjbladaj/zaaw_java/server/service/impl/AccountRegistrationServiceImpl.java
@@ -8,6 +8,7 @@ import pl.mjbladaj.zaaw_java.server.converters.UserDataConverter;
 import pl.mjbladaj.zaaw_java.server.dao.AccountRepository;
 import pl.mjbladaj.zaaw_java.server.dto.UserRegistrationData;
 import pl.mjbladaj.zaaw_java.server.entity.Account;
+import pl.mjbladaj.zaaw_java.server.exceptions.InvalidCredentialsExeption;
 import pl.mjbladaj.zaaw_java.server.exceptions.UsernameOccupiedException;
 import pl.mjbladaj.zaaw_java.server.service.AccountRegistrationService;
 
@@ -18,11 +19,9 @@ public class AccountRegistrationServiceImpl implements AccountRegistrationServic
     private AccountRepository accountRepository;
 
     @Override
-    public UserRegistrationData register(UserRegistrationData userRegistrationData) throws UsernameOccupiedException {
-        if(accountRepository
-                .findByLogin(userRegistrationData.getUsername())
-                .isPresent())
-            throw new UsernameOccupiedException("Username is occupied");
+    public UserRegistrationData register(UserRegistrationData userRegistrationData) throws UsernameOccupiedException, InvalidCredentialsExeption {
+        checkCredentials(userRegistrationData);
+        checkIfUsernameIsUnique(userRegistrationData);
 
         Account newAccount = UserDataConverter
                 .getAccount(userRegistrationData);
@@ -31,7 +30,23 @@ public class AccountRegistrationServiceImpl implements AccountRegistrationServic
                 "{noop}" + newAccount.getPassword()
         );
         accountRepository.save(newAccount);
+
         userRegistrationData.setId(newAccount.getId());
+        userRegistrationData.setPassword("encoded");
         return  userRegistrationData;
+    }
+
+    private void checkIfUsernameIsUnique(UserRegistrationData userRegistrationData) throws UsernameOccupiedException {
+        if(accountRepository
+                .findByLogin(userRegistrationData.getUsername())
+                .isPresent())
+            throw new UsernameOccupiedException("Username is occupied.");
+    }
+
+    private void checkCredentials(UserRegistrationData userRegistrationData) throws InvalidCredentialsExeption {
+        if(userRegistrationData.getUsername().length() < 3)
+            throw new InvalidCredentialsExeption("Username is too short.");
+        if(userRegistrationData.getPassword().length() < 3)
+            throw new InvalidCredentialsExeption("Password is too short.");
     }
 }
